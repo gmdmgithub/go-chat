48f7df38a5ee248e64e2142ed527001bbe048a86
gmdmgithub <gmika@interia.pl>
2019-03-09 21:58:57 +0100

    fornt-end amends

--- static/socket.js
+++ static/socket.js
@@ -1,37 +1,49 @@
-var socket = null;
-var messages = document.querySelectorAll("#messages");
-
-
-document.querySelector("#chatbox").addEventListener('submit', (e) => {
-    e.preventDefault();
-    var msgBox = document.querySelector("#chatbox").querySelector("textarea");
-
-    console.log(`What is goin on? ${msgBox.value}`);
-    
-    if (!msgBox.value) { 
-        alert("Error: Message is empty");
-        return false;
-    }
-    if (!socket) {
-        alert("Error: There is no socket connection.");
-        return false;
-    }
-    socket.send(msgBox.value);
-    msgBox.value = "";
-    return false;
-});
+function addMesage(msg){
+    var node = document.createElement("LI"); // Create a <li> node
+    var textnode = document.createTextNode(msg); // Create a text node
+    node.appendChild(textnode); // Append the text to <li>
+    document.getElementById("messages").appendChild(node); // Append <li> to <ul> with id="myList"
+}
 
 if (!window["WebSocket"]) {
-    alert("Error: Your browser does not support websockets.")
+
+    // alert("Error: Your browser does not support websockets.")
+    addMesage("Error: Your browser does not support websockets.");
+    document.querySelector("#submit").disabled  = true;
+    document.querySelector("#chatbox").querySelector("textarea").disabled  = true;
+    document.querySelector("#submit").style.backgroundColor="gray";
+    document.querySelector("#submit").style.cursor="auto"; 
+
 } else {
+
+    var socket = null;
+    var messages = document.querySelectorAll("#messages");
+
+    document.querySelector("#chatbox").addEventListener('submit', (e) => {
+        e.preventDefault();
+        var msgBox = document.querySelector("#chatbox").querySelector("textarea");
+
+        console.log(`Message is sent ... value: ${msgBox.value}`);
+
+        if (!msgBox.value) {
+            alert("Error: Message is empty");
+            return false;
+        }
+        if (!socket) {
+            alert("Error: There is no socket connection.");
+            return false;
+        }
+        socket.send(msgBox.value);
+        msgBox.value = "";
+        return false;
+    });
+
+
     socket = new WebSocket("ws://localhost:8081/room");
     socket.onclose = () => {
         alert("Connection has been closed.");
     }
     socket.onmessage = (e) => {
-        var node = document.createElement("LI"); // Create a <li> node
-        var textnode = document.createTextNode(e.data); // Create a text node
-        node.appendChild(textnode); // Append the text to <li>
-        document.getElementById("messages").appendChild(node); // Append <li> to <ul> with id="myList"
+        addMesage(e.data);
     }
 }
